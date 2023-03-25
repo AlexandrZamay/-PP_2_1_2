4330e46bb76ded65106c316dff007c93aeee943c
SuzuhaAmane27 <24alexandro24@mail.ru>
2023-03-25 20:03:01 +0300

    Second Try

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String nickname;
+
+    public String voice() {
+        return "meow";
+    }
+
+    public void setColor(String nickname) {
+        this.nickname = nickname;
+    }
+}
