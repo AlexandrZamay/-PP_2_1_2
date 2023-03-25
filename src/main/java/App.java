fb1eb2726fd5e689df998ebb32c353764a5fa1c8
SuzuhaAmane27 <24alexandro24@mail.ru>
2023-03-25 19:43:27 +0300

    First TRy

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = applicationContext.getBean("catBean", Cat.class);
+        Cat cat2 = (Cat) applicationContext.getBean("catBean");
+
+        System.out.println(bean == bean2);
+        System.out.println(cat1 == cat2);
+
     }
 }
